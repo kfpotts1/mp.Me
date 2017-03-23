0d45d2b8efcbabfd1276aad32402c49957e4f7e0
ericjunker <ejunker@ithaca.edu>
2017-03-23 10:36:10 -0400

    Text file controller will soon be able to read lines
    Started testing for the text file controller

--- src/sample/textFileController.java
+++ src/sample/textFileController.java
@@ -1,9 +1,48 @@
 package sample;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Eric on 3/22/17.
  */
 public class textFileController {
     //this bad boy takes care of reading and writing from the text file
     public String fileName = "Options.ini";
+    public String path;
+    public List<String> options= new LinkedList<>();
+
+
+
+
+    public void readFile() throws IOException {
+        //loads the file into an array of lines
+        FileReader fr = new FileReader(path);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+
+        //go through the file, adding lines
+        while (br.readLine() != null) {
+                options.add(br.readLine());
+        }
+        br.close();
+        fr.close();
+    }
+
+    //constructor
+    textFileController(String path) {
+        //get the path of the options file
+        this.path = path;
+        //open file for reading
+        try {
+            readFile();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
 }
---
+++ src/sample/textFileControllerTest.groovy
@@ -0,0 +1,10 @@
+package sample
+
+/**
+ * Created by Eric on 3/23/17.
+ */
+class textFileControllerTest extends GroovyTestCase {
+    public static void main() {
+        //this runs the test suite for the textFileController
+    }
+}
