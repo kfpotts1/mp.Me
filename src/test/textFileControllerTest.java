3dbbc4ea36dc44726577a27df1e37efe4966a260
ericjunker <ejunker@ithaca.edu>
2017-03-24 09:41:36 -0400

    finished a test for reading files. now i need to test the test. should i have written a test for the test before i wrote the test? test driven (test driven) development? how many layers of recursion are there

--- src/test/textFileControllerTest.java
+++ src/test/textFileControllerTest.java
@@ -3,18 +3,37 @@ package test;
 import org.junit.Test;
 import sample.*;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
 
 /**
  * Created by Eric on 3/24/17.
  */
 public class textFileControllerTest {
     @Test
-    public void openFile() {
+    public void readFile() {
         //this tests to see if you can successfully read a file
         //note: this file path only works on my computer
         String path = "/Users/Eric/Dropbox/Software Eng/mp.Me/options";
         //create a textFileController
         textFileController reader = new textFileController(path);
+        //make sure the line menu is empty
+        if (!reader.options.isEmpty()) {
+            System.out.println("Test failed: options list was not empty on open");
+            System.exit(1);
+        }
+        //read the options file
+        try {
+            reader.readFile();
+        } catch (IOException e) {
+            System.out.println(e);
+            System.out.println("Test failed: file not found");
+            System.exit(1);
+        }
+        //now see if lines have been read
+        if (reader.options.isEmpty()) {
+            System.out.println("Test failed: no input was read");
+            System.exit(1);
+        }
+        System.out.println("Reading files test passed");
     }
 }
