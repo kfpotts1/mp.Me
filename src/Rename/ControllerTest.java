d6b3ebf144ad4a009118323d329ab111ad5cc4b7
kfpotts1 <kfpotts1@gmail.com>
2017-03-26 23:58:54 -0400

    rename testing begin

---
+++ src/Rename/ControllerTest.java
@@ -0,0 +1,21 @@
+package Rename;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kennypotts on 3/26/17.
+ */
+public class ControllerTest {
+
+    @Test
+    public void testSetMoveCMD() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCopyCMD() throws Exception {
+
+    }
+}
