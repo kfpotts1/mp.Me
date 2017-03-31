4c204ccbe9dbfc1eadbecf3cd1e3ef1953f4941a
ericjunker <ejunker@ithaca.edu>
2017-03-31 09:31:14 -0400

    trying to find files

--- src/osx_scripts/search.sh
+++ src/osx_scripts/search.sh
@@ -1,3 +1,7 @@
-read fileExtension
-cmd = '*." + $fileExtension
-ls -r | grep $cmd
+#!/usr/bin/env bash
+
+#usage: run search.sh with file extension as argument
+#run it in whatever directory you want to search, it includes
+#sub-directories
+cmd = '*.' + $1
+ls -r | grep cmd
