f39281113d70fb680818b3c16661e372cf71e7e1
Ben <bleblanc2@ithaca.edu>
2017-04-05 09:41:55 -0400

    optionsController

--- src/template/optionsController.java
+++ src/template/optionsController.java
@@ -9,15 +9,17 @@ import javafx.scene.control.Button;
 import java.net.URL;
 import java.util.ResourceBundle;
 import template.ControlledScreen;
+import textfile.textFileController;
 
-public class optionsController implements Initializable, ControlledScreen{
+public class optionsController implements Initializable, ControlledScreen {
     ScreensController myController;
 
     @FXML
     private Button renameBtn;
     @FXML
     Parent root;
-    public void setScreenParent(ScreensController screenParent){
+
+    public void setScreenParent(ScreensController screenParent) {
         myController = screenParent;
     }
 
@@ -25,56 +27,48 @@ public class optionsController implements Initializable, ControlledScreen{
     public String os, input, output, date;
 
 
-   @FXML
-    public void goToRename(ActionEvent event){
+    @FXML
+    public void goToRename(ActionEvent event) {
         myController.setScreen(template.ScreensFramework.screen2ID);
     }
 
     //this function does nothing useful with it's parameters, but it is needed for intellij to stop whining
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        collectOptions();
     }
 
-    //constructor
-    /*public optionsController(String os, String input, String output, String date){
-        this.os = os;
-        this.input = input;
-        this.output = output;
-        this.date = date;
-    }*/
-
-    //set functions are this.variable = stringfield
-    public void setOS(){
+    public void setOS() {
 
 
     }
 
-    public void setInput(){
+    public void setInput(String inputField) {
 
 
     }
 
-    public void setOutput(){
+    public void setOutput(String outputField) {
 
 
     }
 
-    public void setDate(String monthField, String dayField, String yearField){
+    public void setDate(String monthField, String dayField, String yearField) {
         String month, day, year;
-        //set 3 date indicators from stringfields
-        // *temporary fix*
-        month = "";
-        day = "";
-        year = "";
-    //char checks
+        // splits the date into month, day, year
+        String dateString[] = date.split("/");
+        month = dateString[0];
+        day = dateString[1];
+        year = dateString[2];
+        //user sets 3 date indicators from stringfields
+        //char checks for valid date
         //1 or 2 char between ints 1-12
-        if((monthField.length() == 1) || (monthField.length() == 2)) {
+        if ((monthField.length() == 1) || (monthField.length() == 2)) {
             if ((Integer.parseInt(monthField) > 0) && (Integer.parseInt(monthField) < 13)) {
                 month = monthField;
                 //1 or 2 char between ints 1-31
                 if ((dayField.length() == 1) || (monthField.length() == 2)) {
-                    if ((Integer.parseInt(dayField) > 0) && (Integer.parseInt(dayField) < 31)) {
+                    if ((Integer.parseInt(dayField) > 0) && (Integer.parseInt(dayField) < 32)) {
                         day = dayField;
                         //4 char ints above 1970
                         if ((yearField.length() == 4)) {
@@ -88,25 +82,37 @@ public class optionsController implements Initializable, ControlledScreen{
                 }
             }
         }
-        this.date = month + "/" + "/" + day + "/" + year;
+        date = month + "/" + "/" + day + "/" + year;
     }
 
     //get variables from the textFileController
-    public void getOptions(String[] optArr){
-        this.os = optArr[0];
-        this.input = optArr[1];
-        this.output = optArr[2];
-        this.date = optArr[3];
+    public void collectOptions() {
+        textfile.textFileController opt = new textFileController();
+        os = opt.getOS();
+        input = opt.getInput();
+        output = opt.getOutput();
+        date = opt.getDate();
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public String getDate() {
+        return date;
     }
 
     //Calls all set functions
-    public void reload(){
+    public void reload() {
         //send all stringfields into functions
         setOS();
-        setInput();
-        setOutput();
+        //setInput();
+        //setOutput();
         //setDate();
 
     }
-
 }
