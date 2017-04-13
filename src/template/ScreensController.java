7bd88ff79d9676ac19173f8570bfd561fafb2832
wisejackfr0st <wisejackfr0st@gmail.com>
2017-04-13 10:12:14 -0400

    javadoc commenting and removed some unused functions from ScreensFramework and controller

--- src/template/ScreensController.java
+++ src/template/ScreensController.java
@@ -135,21 +135,6 @@ public class ScreensController  extends StackPane {
             return false;
         }
 
-
-        /*Node screenToRemove;
-         if(screens.get(name) != null){   //screen loaded
-         if(!getChildren().isEmpty()){    //if there is more than one screen
-         getChildren().add(0, screens.get(name));     //add the screen
-         screenToRemove = getChildren().get(1);
-         getChildren().remove(1);                    //remove the displayed screen
-         }else{
-         getChildren().add(screens.get(name));       //no one else been displayed, then just show
-         }
-         return true;
-         }else {
-         System.out.println("screen hasn't been loaded!!! \n");
-         return false;
-         }*/
     }
 
     //This method will remove the screen with the given name from the collection of screens
