a2c5952caa1fe72ef52691b321c734f5ca7b7263
wisejackfr0st <wisejackfr0st@gmail.com>
2017-04-18 09:45:27 -0400

    several updates to splashcontroller and screen (no one touch -- very broken)

--- src/template/splashController.java
+++ src/template/splashController.java
@@ -38,6 +38,8 @@ public class splashController implements Initializable, ControlledScreen{
     ScreensController myController;
 
 
+
+
     @FXML
     public ToggleGroup operatingSystemButtons;
     @FXML
